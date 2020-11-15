4170f534d6b0108a8edcde9b0850f509d5b6fe6a
TEDIORELEE <tediorelee@iidx.xyz>
2020-11-15 21:46:22 +0800

    install ant-design

---
+++ src/plugins/ant-design.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from 'ant-design-vue/lib/button'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.component(Button.name, Button)
+
+Vue.config.productionTip = false
